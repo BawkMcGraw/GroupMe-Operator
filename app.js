f250b1957a2a7c7da8490e435f2943440935c7f2
Justin McKee <bawk@Justins-MacBook-Pro.local>
2020-03-27 13:32:00 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const director = require('director');
+const Bot = require('./bot');
+
+class Server{
+
+    //HANDLES DATA FROM GROUPME ON CALL
+    constructor(router, port) {
+        this.server = http.createServer(function(req, res) {
+            req.chunks = [];
+            req.on('data', function(chunk) {
+                req.chunks.push(chunk.toString());
+            });
+
+            //HANDLES ERRORS FOR CALL
+            router.dispatch(req, res, function(err) {
+                res.writeHead(err.status, {'Content-Type': 'text/plain'});
+                res.end(err.message);
+            });
+        });
+
+        this.port = Number(process.env.port || 3000);
+    };
+
+    //BUILDS SERVER PORT THAT LISTENS FOR GROUPME CALLS
+    serve() {
+        this.server.listen(this.port);
+        console.log(`Port: ${this.port}`);
+    };
+
+    static getResponse() {
+        this.res.end('');
+    };
+
+    //PARSES AND REACTS TO GROUPME DATA
+    static postReponse() {
+        const reqMessage = JSON.parse(this.req.chunks[0]);
+
+        this.res.writeHead(200);
+        this.res.end();
+
+        //ACTUAL BOT
+        const resMessage = Bot.Bot.checkMessage(reqMessage);
+        if (resMessage) {
+
+            //STARTS SEND
+            Bot.Bot.sendMessage(resMessage);
+        }
+    };
+};
+
+//BUILDS SERVER THAT LISTENS FOR GROUPME CALLS
+const router = new director.http.Router({
+    '/': {
+        post: Server.postReponse,
+        get: Server.getResponse
+    }
+});
+
+//STARTS SERVER
+const server = new Server(router);
+server.serve();
---
+++ bot.js
@@ -0,0 +1,80 @@
+const https = require('https');
+const nameex = /Operator/;
+const atex = /@everyone/i;
+const atex2 = /@all/i;
+// USER IDS FROM EVERYONE IN GROUP
+var users = ["33073287", "26997134", "29962743", "31154730", "48138508"];
+
+class Functions {
+
+}
+class Bot {
+
+    // CHECKS MESSAGE FOR @EVERYONE OR @ALL
+    static checkMessage(message) {
+        var mT = message.text;
+        var mN = message.name;
+        var UID = new RegExp(message.user_id);
+
+        // MAKES SURE BOT DOES NOT TRIGGER BOT
+        if (nameex.test(mN)) {
+            return null;
+        }
+        if (atex.test(mT) || atex2.test(mT)) {
+
+            // REMOVES USER WHO CALLED FROM MENTIONS LIST
+            for (i=0; i<users.length; i++) {
+                if (UID.test(users[i])) {
+                    users.splice(i, 1);
+                    return 'Connecting...';
+                }
+            }
+        }
+    }
+    // COMPOSES AND SENDS MESSAGE
+    static sendMessage(message) {
+
+        // DETERMINES BOTID FOR GROUP THAT CALLED
+        var botId;
+        const botids = ['a77921fc68936cf5c8fa6e58a3','1'];
+        const groups = [/27754904/,/1/];
+        for (i=0; i<groups.length; i++) {
+            if (groups[i].test(message.group_id)) {
+                botId = botids[i];
+            }
+        }
+
+        const options = {
+            hostname: 'api.groupme.com',
+            path: '/v3/bots/post',
+            method: 'POST'
+        };
+
+        //BUILDS INFORMATION SENT TO GROUPME
+        const body = {
+            bot_id: botId,
+            text: message,
+            attachments: {
+                type: "mentions",
+                user_ids: users
+            }
+        };
+
+        //CREATES SERVER REQUEST AND POST
+        const botReq = https.request(options, function(res) {
+            if (res.statusCode !== 202) {
+                console.log(`Error: ${res.statusCode} ${res.statusMessage}`);
+            }
+        });
+
+        botReq.on('error', function(err) {
+            console.log(`Error: ${JSON.stringify(err)}`);
+        });
+
+        botReq.on('timeout', function(err) {
+            console.log(`Timeout: ${JSON.stringify(err)}`);
+        });
+        botReq.end(JSON.stringify(body));
+    }
+}
+module.exports = {Bot:Bot, Functions:Functions}
